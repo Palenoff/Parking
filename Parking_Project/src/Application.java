cb9bbf460024e6dc7a4d063474af0414ecd22a20
Kirill Palenov <kpalenov@mail.ru>
2019-03-07 17:31:21 +0300

    Revert "Encoding fixed"
    This reverts commit 754f083d13193f0810bbf30e7a8144f9f3307db3.

--- Parking_Project/src/Application.java
+++ Parking_Project/src/Application.java
@@ -9,15 +9,15 @@ public class Application {
 	
 	public static void menu() throws Exception {
 		while(true) {
-			System.out.println("\n\nРЎРїРёСЃРѕРє РєРѕРјР°РЅРґ:\n"
-					+ "p:N - РїСЂРёРїР°СЂРєРѕРІР°С‚СЊ РјР°С€РёРЅСѓ, РіРґРµ N - РєРѕР»РёС‡РµСЃС‚РІРѕ РјР°С€РёРЅ РЅР° РІСЉРµР·Рґ\r\n" + 
-					"u:N - РІС‹РµС…Р°С‚СЊ СЃ РїР°СЂРєРѕРІРєРё, РіРґРµ N - РЅРѕРјРµСЂ РїР°СЂРєРѕРІРѕС‡РЅРѕРіРѕ Р±РёР»РµС‚Р°\r\n" + 
-					"u:[1..n] - РІС‹РµС…Р°С‚СЊ СЃ РїР°СЂРєРѕРІРєРё РЅРµСЃРєРѕР»СЊРєРёРј РјР°С€РёРЅР°Рј, РіРґРµ РІ РєРІР°РґСЂР°С‚РЅС‹С… СЃРєРѕР±РєР°С…, С‡РµСЂРµР· Р·Р°РїСЏС‚СѓСЋ РїРµСЂРµРґР°СЋС‚СЃСЏ РЅРѕРјРµСЂР° РїР°СЂРєРѕРІРѕС‡РЅС‹С… Р±РёР»РµС‚РѕРІ\r\n" + 
-					"l - СЃРїРёСЃРѕРє РјР°С€РёРЅ, РЅР°С…РѕРґСЏС‰РёС…СЃСЏ РЅР° РїР°СЂРєРѕРІРєРµ\r\n" +
-					"c - РєРѕР»РёС‡РµСЃС‚РІРѕ РѕСЃС‚Р°РІС€РёС…СЃСЏ РјРµСЃС‚ РЅР° РїР°СЂРєРѕРІРєРµ\r\n" + 
-					"s - РЅР°СЃС‚СЂРѕР№РєРё\r\n" + 
-					"e - РІС‹С…РѕРґ РёР· РїСЂРёР»РѕР¶РµРЅРёСЏ\r\n" + 
-					"Р’РІРµРґРёС‚Рµ РєРѕРјР°РЅРґСѓ:");
+			System.out.println("\n\nСписок команд:\n"
+					+ "p:N - припарковать машину, где N - количество машин на въезд\r\n" + 
+					"u:N - выехать с парковки, где N - номер парковочного билета\r\n" + 
+					"u:[1..n] - выехать с парковки нескольким машинам, где в квадратных скобках, через запятую передаются номера парковочных билетов\r\n" + 
+					"l - список машин, находящихся на парковке\r\n" +
+					"c - количество оставшихся мест на парковке\r\n" + 
+					"s - настройки\r\n" + 
+					"e - выход из приложения\r\n" + 
+					"Введите команду:");
 			System.out.print("> ");
 			BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
 			try {
@@ -28,7 +28,7 @@ public class Application {
 		               {
 		            	   int c = Integer.parseInt(choice.substring(2));
 		            	   park(c);
-			        	   System.out.println("Р’СЃРµ РјР°С€РёРЅС‹ Р·Р°РµС…Р°Р»Рё РЅР° РїР°СЂРєРѕРІРєСѓ!\n");
+			        	   System.out.println("Все машины заехали на парковку!\n");
 		               }
 		               break;
 		           case ('u'):
@@ -46,73 +46,73 @@ public class Application {
 		               }		            	   
 		               break;
 		           case ('l'):
-		        	   System.out.println("РЎРїРёСЃРѕРє РјР°С€РёРЅ РЅР° РїР°СЂРєРѕРІРєРµ!\n");
+		        	   System.out.println("Список машин на парковке!\n");
 		           		if (Parking.getUsed_tickets().isEmpty())
-		           			System.out.println("РњР°С€РёРЅ РЅР° РїР°СЂРєРѕРІРєРµ РЅРµС‚:\n");
+		           			System.out.println("Машин на парковке нет:\n");
 		           		else
 		           			for (int ticket_number : Parking.getUsed_tickets().keySet()) {
 		           				System.out.println(Parking.getUsed_tickets().get(ticket_number).getCar().getSign() + " - " + Parking.getUsed_tickets().get(ticket_number).getNumber());
 		        		}
 		               break;
 		           case ('c'):
-		        	   System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРІРѕР±РѕРґРЅС‹С… РјРµСЃС‚ РЅР° РїР°СЂРєРѕРІРєРµ: " + Parking.getUnused_tickets().size() + "\n\n");
+		        	   System.out.println("Количество свободных мест на парковке: " + Parking.getUnused_tickets().size() + "\n\n");
 		               break;
 		           case ('e'):
 		               System.exit(0);
 		               break;
 		           case ('s'):
-		        	   System.out.println("Р’С‹Р±РµСЂРёС‚Рµ РїР°СЂР°РјРµС‚СЂ:\nСЃ - РІРјРµСЃС‚РёРјРѕСЃС‚СЊ РїР°СЂРєРёРЅРіР°\ns - РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ РЅР° Р·Р°РµР·Рґ");
+		        	   System.out.println("Выберите параметр:\nс - вместимость паркинга\ns - количество секунд на заезд");
 		           	   System.out.print("> ");
 		           	   BufferedReader settings_reader =  new BufferedReader(new InputStreamReader(System.in));
 		           	   char c = (char) settings_reader.read();
 		           	   switch (c) {
 		           	   case ('c'):
-		           		   System.out.println("Р’РјРµСЃС‚РёРјРѕСЃС‚СЊ РїР°СЂРєРёРЅРіР°: " + Parking.getCapacity() + "\nРР·РјРµРЅРёС‚СЊ?\n1 - Р”Р°\n0 - РќРµС‚");
+		           		   System.out.println("Вместимость паркинга: " + Parking.getCapacity() + "\nИзменить?\n1 - Да\n0 - Нет");
 		           	   	   System.out.print("> ");
 		           	   	   BufferedReader capacity_settings_reader =  new BufferedReader(new InputStreamReader(System.in));
 		           	   	   int cc = capacity_settings_reader.read();
 		           	   	   switch (cc) {
 		           	   	   case ('1'): 
 		           	   		   setParkingCapacity();
-		           	   	   	   System.out.println("РќРѕРІР°СЏ РІРјРµСЃС‚РёРјРѕСЃС‚СЊ РїР°СЂРєРёРЅРіР° Р·Р°РґР°РЅР°!");
+		           	   	   	   System.out.println("Новая вместимость паркинга задана!");
 		           	   	       break;
 		           	   	   case ('0'):
-		           	   		   System.out.println("РР·РјРµРЅРµРЅРёРµ РІРјРµСЃС‚РёРјРѕСЃС‚Рё РїР°СЂРєРёРЅРіР° РѕС‚РјРµРЅРµРЅРѕ!");
+		           	   		   System.out.println("Изменение вместимости паркинга отменено!");
 		           	   		   break;
 		           	   	   default:
-				               throw new IOException("РљРѕРјР°РЅРґР° СѓРєР°Р·Р°РЅР° РЅРµРІРµСЂРЅРѕ");
+				               throw new IOException("Команда указана неверно");
 		           	   	   }
 		           		   break;
 		           	   case ('s'):
-		           		   System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ РЅР° Р·Р°РµР·Рґ: " + Parking.getSeconds()+ "\nРР·РјРµРЅРёС‚СЊ?\n1 - Р”Р°\n0 - РќРµС‚\n");
+		           		   System.out.println("Количество секунд на заезд: " + Parking.getSeconds()+ "\nИзменить?\n1 - Да\n0 - Нет\n");
 		           	   	   System.out.print("> ");
 	           	   	   	   BufferedReader seconds_settings_reader =  new BufferedReader(new InputStreamReader(System.in));
 		           	   	   int cs = seconds_settings_reader.read();
 		           	   	   switch (cs) {
 		           	   	   case ('1'): 
 		           	   		   setCarTimer();
-		           	   	   	   System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ Р·Р°РґР°РЅРѕ");
+		           	   	   	   System.out.println("Количество секунд задано");
 		           	   	       break;
 		           	   	   case ('0'):
-		           	   		   System.out.println("РР·РјРµРЅРµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЃРµРєСѓРЅРґ РЅР° Р·Р°РµР·Рґ РѕС‚РјРµРЅРµРЅРѕ!");
+		           	   		   System.out.println("Изменение количества секунд на заезд отменено!");
 		           	   		   break;
 		           	   	   default:
-				               throw new IOException("РљРѕРјР°РЅРґР° СѓРєР°Р·Р°РЅР° РЅРµРІРµСЂРЅРѕ");
+				               throw new IOException("Команда указана неверно");
 		           	   	   }
 		           		   break;
 		           	   default:
-			               throw new IOException("РљРѕРјР°РЅРґР° СѓРєР°Р·Р°РЅР° РЅРµРІРµСЂРЅРѕ");
+			               throw new IOException("Команда указана неверно");
 		           	   }
 		               break;
 		           default:
-		               throw new IOException("РљРѕРјР°РЅРґР° СѓРєР°Р·Р°РЅР° РЅРµРІРµСЂРЅРѕ");
+		               throw new IOException("Команда указана неверно");
 		       }
 			} 
 			catch (IOException e) {
 				System.out.println("\n\n" + e.getMessage() + "\n\n");
 			}
 			finally {
-				System.out.println("\nР”Р»СЏ РїСЂРѕРґРѕР»Р¶РµРЅРёСЏ РЅР°Р¶РјРёС‚Рµ Enter");
+				System.out.println("\nДля продолжения нажмите Enter");
 				new BufferedReader(new InputStreamReader(System.in)).readLine();
 			}
 		}
@@ -126,7 +126,7 @@ public class Application {
 	}
 	
 	public static void setParkingCapacity() {
-		System.out.println("Р’РІРµРґРёС‚Рµ, РєРѕР»РёС‡РµСЃС‚РІРѕ РјР°С€РёРЅРѕРјРµСЃС‚ РґР»СЏ РїР°СЂРєРёРЅРіР°: ");
+		System.out.println("Введите, количество машиномест для паркинга: ");
 		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
 		int parking_capacity;
 		boolean is_set_capacity = false;
@@ -135,12 +135,12 @@ public class Application {
 					String parking_capacity_str = reader.readLine();
 					parking_capacity = Integer.parseInt(parking_capacity_str);
 					if (parking_capacity < 0)
-						throw new IllegalArgumentException("РљРѕР»РёС‡РµСЃС‚РІРѕ РјРµСЃС‚ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹Рј. РџРѕРїСЂРѕР±СѓР№С‚Рµ РµС‰С‘ СЂР°Р·: ");
+						throw new IllegalArgumentException("Количество мест не может быть отрицательным. Попробуйте ещё раз: ");
 					Parking.setCapacity(parking_capacity);
 					is_set_capacity = true;
 				}
 		        catch (NumberFormatException e) {
-		        	System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ РјРµСЃС‚ РЅР° РїР°СЂРєРѕРІРєРµ Р·Р°РґР°РЅРѕ РЅРµРІРµСЂРЅРѕ. РџРѕРїСЂРѕР±СѓР№С‚Рµ РµС‰С‘ СЂР°Р·: ");
+		        	System.out.println("Количество мест на парковке задано неверно. Попробуйте ещё раз: ");
 		        }
 				catch (IllegalArgumentException e) {
 		        	System.out.println(e.getMessage());
@@ -153,7 +153,7 @@ public class Application {
 	}
 	
 	public static void setCarTimer() {
-		System.out.println("Р’РІРµРґРёС‚Рµ, РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ РЅР° РІСЉРµР·Рґ РґР»СЏ РјР°С€РёРЅС‹: ");
+		System.out.println("Введите, количество секунд на въезд для машины: ");
 		BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
 		int car_seconds;
 		boolean is_set_seconds = false;
@@ -162,14 +162,14 @@ public class Application {
 					String car_seconds_str = reader.readLine();
 					car_seconds = Integer.parseInt(car_seconds_str);
 					if (car_seconds < 0)
-						throw new IllegalArgumentException("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹Рј. РџРѕРїСЂРѕР±СѓР№С‚Рµ РµС‰С‘ СЂР°Р·: ");
+						throw new IllegalArgumentException("Количество секунд не может быть отрицательным. Попробуйте ещё раз: ");
 					if (car_seconds > 6)
-						throw new IllegalArgumentException("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ Р±РѕР»РµРµ РїСЏС‚Рё. РџРѕРїСЂРѕР±СѓР№С‚Рµ РµС‰С‘ СЂР°Р·: ");
+						throw new IllegalArgumentException("Количество секунд не может быть более пяти. Попробуйте ещё раз: ");
 					Parking.setSeconds(car_seconds);
 					is_set_seconds = true;
 				}
 		        catch (NumberFormatException e) {
-		        	System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРµРєСѓРЅРґ РґР»СЏ РІСЉРµР·РґР° РјР°С€РёРЅС‹ РЅР° РїР°СЂРєРѕРІРєСѓ Р·Р°РґР°РЅРѕ РЅРµРІРµСЂРЅРѕ. РџРѕРїСЂРѕР±СѓР№С‚Рµ РµС‰С‘ СЂР°Р·: ");
+		        	System.out.println("Количество секунд для въезда машины на парковку задано неверно. Попробуйте ещё раз: ");
 		        }
 				catch (IllegalArgumentException e) {
 		        	System.out.println(e.getMessage());
@@ -183,7 +183,7 @@ public class Application {
 	
 	public static void main(String[] args)  {
 		try {
-		System.out.println("Р—РґСЂР°РІСЃС‚РІСѓР№С‚Рµ!\nР”РѕР±СЂРѕ РїРѕР¶Р°Р»РѕРІР°С‚СЊ РІ РїСЂРёР»РѕР¶РµРЅРёРµ РџР°СЂРєРёРЅРі!");
+		System.out.println("Здравствуйте!\nДобро пожаловать в приложение Паркинг!");
 		Parking.setCapacity(100);
 		Parking.setSeconds(3);
 		menu();		
